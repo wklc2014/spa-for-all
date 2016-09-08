baed3095981ec790f90a50456dca1d5a03516ed8
wb-wk228937 <wb-wk228937@ant-financial.com>
2016-09-08 09:37:20 +0800

    add mocha test project

---
+++ project/mocha-test/src/js/view/App.jsx
@@ -0,0 +1,26 @@
+'use strict';
+
+import React, {Component} from 'react';
+import NavLink from './common/NavLink.jsx';
+
+class App extends Component{
+	render(){
+		return (
+			<div className="container">
+				<ul className="nav nav-tabs">
+					<li role="presentation">
+						<NavLink to="/" onlyActiveOnIndex>首页</NavLink>
+					</li>
+					<li role="presentation">
+						<NavLink to="/first">First</NavLink>
+					</li>
+				</ul>
+				<section>
+					{this.props.children}
+				</section>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ project/mocha-test/src/js/view/First.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+class First extends Component{
+	render(){
+		return (
+			<div>
+				this is default page
+			</div>
+		)
+	}
+}
+
+export default First;
