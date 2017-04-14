beb750737af6b456c999fe49fca034cfae39e239
wangkun <wb-wk228937@ant-financial.com>
2017-04-14 18:27:21 +0800

    use antd

--- project/reactDemo/src/component/App.jsx
+++ project/reactDemo/src/component/App.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log(111);
+    }
+
     render() {
         return (
             <div>
-                this is app component
+                <p>
+                    this is app component
+                </p>
+                <p>
+                    <Button
+                        type="primary"
+                        onClick={this.handleClick}
+                    >
+                        按钮
+                    </Button>
+                </p>
             </div>
         )
     }
