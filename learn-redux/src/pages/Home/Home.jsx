96d25dc83eb8c631983f87005477ee25398e52bf
wklc2014 <373658358@qq.com>
2018-10-03 17:04:32 +0800

    [master]

--- learn-redux/src/pages/Home/Home.jsx
+++ learn-redux/src/pages/Home/Home.jsx
@@ -1,82 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { Button } from 'antd';
 import CountContext from '../../models/context/count_context.js';
 import Parent from './Paren.jsx';
 import count_action from '../../models/action/count_action.js';
 import getDataBase64ByCanvas from '../../utils/getDataBase64ByCanvas.js';
+import picture from './common/6.jpg';
 
-function Home (props) {
+class Home extends Component {
 
-  const {
-    amount,
-    citys,
-    loading,
-    dispatch,
-  } = props;
-
-  const CountContextValue = {
-    amount,
-    onAdd: (number) => {
-      dispatch(count_action.onAdd(number));
-    },
-    onReduce: (number) => {
-      dispatch(count_action.onReduce(number));
-    },
+  static defaultProps = {
+    amount: 0,
+    loading: false,
+    citys: [],
   }
 
-  const onLoad = () => {
-    dispatch(count_action.onLoad());
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: '',
+    }
   }
 
-  const imgSrc = getDataBase64ByCanvas({
-    text: '大成都',
-    font: {
-      family: 'Microsoft YaHei',
-    },
-    mask: {
-      opacity: 0.5
-    },
-  });
+  componentDidMount() {
+    const imgInstantce = new Image();
+    imgInstantce.src = picture;
+    imgInstantce.onload = () => {
+      const imgSrc = getDataBase64ByCanvas({
+        text: '大成都',
+        // maskOpacity: 0.5,
+        backgroundImage: imgInstantce,
+      });
+      this.setState({ imgSrc });
+    }
+  }
 
-  return (
-    <CountContext.Provider value={CountContextValue}>
-      <div>
-        <h2>主页</h2>
-        <p>这是主页内容。。。</p>
+  onAdd = (number) => {
+    this.props.dispatch(count_action.onAdd(number));
+  }
+
+  onReduce = (number) => {
+    this.props.dispatch(count_action.onReduce(number));
+  }
+
+  onLoad = () => {
+    this.props.dispatch(count_action.onLoad());
+  }
+
+  render() {
+    const {
+      amount,
+      citys,
+      loading,
+    } = this.props;
+
+    const {
+      imgSrc,
+    } = this.state;
+
+    const CountContextValue = {
+      amount,
+      onAdd: this.onAdd,
+      onReduce: this.onReduce,
+    }
+
+    const divStyle = {
+      // background: '#777',
+      border: '1px solid #ddd',
+    }
+
+    return (
+      <CountContext.Provider value={CountContextValue}>
         <div>
-          <img src={imgSrc} alt=""/>
+          <h2>主页</h2>
+          <p>这是主页内容。。。</p>
+          <div style={divStyle}>
+            <img src={imgSrc} alt=""/>
+          </div>
+          <Parent />
         </div>
-        <Parent />
-      </div>
-      <div style={{ padding: '16px 0' }}>
-        <Button
-          type="primary"
-          loading={loading}
-          onClick={onLoad}
-        >
-          加载
-        </Button>
-      </div>
-      <div style={{ padding: '16px 0' }}>
-        <ul className="ui list">
-          {citys.map((c, i) => <li key={i}>{c}</li>)}
-        </ul>
-      </div>
-    </CountContext.Provider>
-  )
+        <div style={{ padding: '16px 0' }}>
+          <Button
+            type="primary"
+            loading={loading}
+            onClick={this.onLoad}
+          >
+            加载
+          </Button>
+        </div>
+        <div style={{ padding: '16px 0' }}>
+          <ul className="ui list">
+            {citys.map((c, i) => <li key={i}>{c}</li>)}
+          </ul>
+        </div>
+      </CountContext.Provider>
+    )
+  }
 }
 
 Home.propTypes = {
   amount: propTypes.number,
   citys: propTypes.array,
   loading: propTypes.bool,
-}
-
-Home.defaultProps = {
-  amount: 0,
-  loading: false,
-  citys: [],
 }
 
 export default Home;
--- learn-redux/src/utils/getDataBase64ByCanvas.js
+++ learn-redux/src/utils/getDataBase64ByCanvas.js
@@ -1,63 +1,68 @@
 /**
  * 通过 canvas 将文字转换成图像数据 base64
  */
-export default function getDataBase64ByCanvas(params = {}) {
-  const {
-    text = 'canvas',
-    font = {},
-    mask = {},
-  } = params;
-
-  // 文本配置
-  const fontSetting = Object.assign({}, {
-    size: 20,
-    color: '#000',
-    family: 'Arial',
-    align: 'middle',
-    rotate: -10,
-  }, font);
-
-  // 图片配置
-  const maskSetting = Object.assign({}, {
-    color: '#fff',
-    width: 200,
-    height: 100,
-    opacity: 1,
-    type: 'png',
-  }, mask);
+function getDataBase64ByCanvas(params = {}) {
+  const settings = Object.assign({}, {
+    // 文本内容
+    text: 'canvas',
+    // 文本对其
+    textAlign: 'middle',
+    // 文本旋转角度
+    textRotate: -20,
+    // 文本颜色
+    fontColor: '#000',
+    // 文本字体
+    fontFamily: 'Arial',
+    // 文本大小
+    fontSize: 20,
+    // 背景颜色
+    backgroundColor: '#fff',
+    // 背景图像
+    backgroundImage: null,
+    // 生成图像宽度
+    maskWidth: 200,
+    // 生成图像高度
+    maskHeight: 100,
+    // 生成图像透明度
+    maskOpacity: 1,
+  }, params);
 
   // 创建 canvas 标签
   const canvas = document.createElement('canvas');
 
   // 设置 canvas 宽度和高度
-  canvas.width = maskSetting.width;
-  canvas.height = maskSetting.height;
+  canvas.width = settings.maskWidth;
+  canvas.height = settings.maskHeight;
 
   // canvas 绘制对象
   const context = canvas.getContext('2d');
 
   // 设置透明度
-  context.globalAlpha = maskSetting.opacity;
+  context.globalAlpha = settings.maskOpacity;
 
   // 绘制矩形作为背景颜色
-  if (maskSetting.color) {
-    context.fillStyle = maskSetting.color;
-    context.fillRect(0, 0, maskSetting.width, maskSetting.height);
+  if (settings.backgroundImage) {
+    context.drawImage(settings.backgroundImage, 0, 0, settings.maskWidth, settings.maskHeight);
+  } else if (settings.backgroundColor) {
+    context.fillStyle = settings.backgroundColor;
+    context.fillRect(0, 0, settings.maskWidth, settings.maskHeight);
   }
 
   // 移动 canvas 中心
-  context.translate(maskSetting.width / 2, maskSetting.height / 2);
-  context.rotate(fontSetting.rotate * Math.PI / 180);
+  context.translate(settings.maskWidth / 2, settings.maskHeight / 2);
+  context.rotate(settings.textRotate * Math.PI / 180);
 
   // 绘制文本
-  context.fillStyle = fontSetting.color;
-  context.font = `${fontSetting.size}px ${fontSetting.family}`;
-  const textWidth = context.measureText(text).width;
-  context.fillText(text, - textWidth / 2, 0);
-  context.textBaseline = fontSetting.align;
+  context.fillStyle = settings.fontColor;
+  context.font = `${settings.fontSize}px ${settings.fontFamily}`;
+  const textWidth = context.measureText(settings.text).width;
+  context.fillText(settings.text, - textWidth / 2, 0);
+  context.textBaseline = settings.textAlign;
 
   //注意这里背景透明的话，需要使用png
-  const dataUrl = canvas.toDataURL(`image/${maskSetting.type}`);
+  const dataUrl = canvas.toDataURL('image/png');
 
   return dataUrl;
 }
+
+export default getDataBase64ByCanvas;
